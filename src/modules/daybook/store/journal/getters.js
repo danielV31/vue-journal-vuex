1f691ba3a320137d6c3168804adeff877cbbed93
danielV31 <daniel.valencia@academia.unimeta.edu.co>
2023-04-04 15:24:51 -0500

    Primer Commit

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,7 @@
+// export const myGetters = (state) => {
+// return StaticRange.algo
+// }
+
+export const getEntriesByTerm = (/*state*/) => {};
+
+export const getEntriesById = (/*state*/) => {};
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Ea magna qui ex mollit laboris ex laboris enim quis reprehenderit. Ut magna aliqua quis nulla ut. Excepteur aute adipisicing consectetur ad incididunt laboris cillum nulla officia tempor est ullamco. Exercitation irure exercitation consequat est anim minim commodo.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "Dolore dolor ad nostrud id enim. Labore laboris officia labore deserunt ex mollit mollit ea est nisi sint aliquip. Ex id sunt do veniam sunt labore et sit cupidatat dolor magna. Ullamco proident sit minim dolore aliquip nostrud sit.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: "Elit eu ut dolor et quis et quis veniam do in aute. Nisi dolore magna sint dolore laboris in ad mollit veniam aliqua mollit laboris Lorem eu. Amet Lorem eu ullamco adipisicing ea aute est proident laboris sit irure dolore ad anim. Adipisicing anim ipsum labore labore. Lorem do laboris anim eu minim fugiat aliqua.",
+      picture: null,
+    },
+  ],
+});
